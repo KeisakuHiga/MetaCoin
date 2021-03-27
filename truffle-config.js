92b432d96e4c1494e20d73b1545b9549aecdcba0
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2021-03-27 18:18:58 +0900

    trying to deploy contract to test net

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,5 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+require('dotenv').config()
 module.exports = {
   // Uncommenting the defaults below 
   // provides for an easier quick-start with Ganache.
@@ -15,6 +17,19 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*"
+    },
+    rinkeby: {
+      provider: function() { 
+        return new HDWalletProvider(process.env.MNEMONIC, `https://rinkeby.infura.io/v3/${process.env.RINKEBY_API}`);
+      },
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
+    },
+  },
+  compilers: {
+    solc: {
+      version: "0.5.8"
     }
   }
 };
